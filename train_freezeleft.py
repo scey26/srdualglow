6ecc4674875fa87df1fb7b593fdfbd8ca6fb2a08
scey26 <scey26@naver.com>
2021-07-07 16:01:16 +0900

    ref_srflow2

--- train_freezeleft.py
+++ train_freezeleft.py
@@ -18,18 +18,18 @@ import os
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 parser = argparse.ArgumentParser(description="Glow trainer")
-parser.add_argument("--batch", default=16, type=int, help="batch size")
+parser.add_argument("--batch", default=4, type=int, help="batch size")
 parser.add_argument("--iter", default=200000, type=int, help="maximum iterations")
 parser.add_argument("--n_flow", default=32, type=int, help="number of flows in each block")
 parser.add_argument("--n_block", default=4, type=int, help="number of blocks")
 parser.add_argument("--no_lu", action="store_true", help="use plain convolution instead of LU decomposed version")
-parser.add_argument("--affine", action="store_true", help="use affine coupling instead of additive")
+parser.add_argument("--affine", action="store_false", help="use affine coupling instead of additive")
 parser.add_argument("--n_bits", default=5, type=int, help="number of bits")
 parser.add_argument("--lr", default=1e-4, type=float, help="learning rate")
 parser.add_argument("--img_size", default=128, type=int, help="image size")
 parser.add_argument("--scale", default=2, type=int, help="SR scale")
 parser.add_argument("--temp", default=0.7, type=float, help="temperature of sampling")
-parser.add_argument("--n_sample", default=1, type=int, help="number of samples")
+parser.add_argument("--n_sample", default=4, type=int, help="number of samples") # it should be same with batch size
 parser.add_argument("path", metavar="PATH", type=str, help="Path to image directory")
 parser.add_argument("--save_folder", type=str)
 
@@ -220,7 +220,7 @@ if __name__ == "__main__":
     cond_shapes = calc_cond_shapes(3, [args.img_size, args.img_size], args.n_block)
 
     model_single_left = Glow(
-        3, args.n_flow, args.n_block, affine=args.affine, conv_lu=not args.no_lu
+        3, args.n_flow, args.n_block, affine=False, conv_lu=not args.no_lu
     )
     model_left = nn.DataParallel(model_single_left)
     # model = model_single
